80193890a0e70daf711236e66c36fbef71ec4932
Rikith Reddy <rikithreddy03@gmail.com>
2021-09-18 00:38:34 +0530

    player: Restirict player movement

--- player.gd
+++ player.gd
@@ -2,18 +2,18 @@ extends KinematicBody2D
 
 # Move to settings
 const BLOCK_DIST = 110
+var WIDTH
+var HEIGHT
 
 func _ready():
 	# Use settings constant
-	var WIDTH = OS.get_window_size().x
-	var HEIGHT = OS.get_window_size().y
+	WIDTH = OS.get_window_size().x
+	HEIGHT = OS.get_window_size().y
 
 func _physics_process(delta):
 	
-	if Input.is_action_just_pressed("right"):
-		print(self.position)
+	if Input.is_action_just_pressed("right") && (self.position.x + BLOCK_DIST) < WIDTH:
 		self.position.x += BLOCK_DIST
 
-	if Input.is_action_just_pressed("left"):
+	if Input.is_action_just_pressed("left") && (self.position.x - BLOCK_DIST) > 0:
 		self.position.x -= BLOCK_DIST
-		print(self.position.x)
