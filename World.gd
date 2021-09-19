18c9a2a6c5977eb0f3da2ad5b1e51b4d1d474ae1
Rikith Reddy <rikithreddy03@gmail.com>
2021-09-19 21:48:02 +0530

    ui: Add UI background

--- World.gd
+++ World.gd
@@ -4,6 +4,4 @@ const PLAYER_POSITION = Vector2(70, 500)
 
 func _ready():
 	$player.position = PLAYER_POSITION
-	$Background.margin_right = 360 # Use settings constant
-	
-
+	$Control/Background.margin_right = 360 # Use settings constant
